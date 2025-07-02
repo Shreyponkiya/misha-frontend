45339cd2ca9e8cf56c54d5c187b73c7781744b86
Shreyponkiya <shreyponkiya@gmail.com>
2025-07-02 11:53:46 +0530

    Update UpdateProduct.jsx

--- src/components/product/UpdateProduct.jsx
+++ src/components/product/UpdateProduct.jsx
@@ -1195,7 +1195,7 @@ const UpdateProduct = ({
                 onClick={addCollection}
                 className={`px-4 py-2 rounded-md transition-colors duration-200 hover:cursor-pointer${
                   updating || loading
-                    ? "bg-gray-400 cursor-not-allowed"
+                    ? "bg-blue-400 cursor-not-allowed"
                     : "bg-blue-600 text-white hover:bg-blue-700 focus:ring-2 focus:ring-blue-500"
                 }`}
                 disabled={updating || loading}
